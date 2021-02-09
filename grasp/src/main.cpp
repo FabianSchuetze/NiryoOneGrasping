1fae9718c8401e4c84644a9edfbf1ae1860c8caa
FabianSchuetze <fabian.schutze@eui.eu>
2021-02-09 12:48:51 +0100

    rewritten to contain a vector

--- grasp/src/main.cpp
+++ grasp/src/main.cpp
@@ -6,27 +6,37 @@
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <tf2_ros/transform_listener.h>
 
-geometry_msgs::Pose convert_message(const gpd_ros::GraspConfigList& msg) {
-    gpd_ros::GraspConfig first = msg.grasps[0];
-    geometry_msgs::Pose pose;
-    pose.position = first.position;
+std::vector<geometry_msgs::Pose> convert_message(
+    const gpd_ros::GraspConfigList& msg) {
+    std::vector<geometry_msgs::Pose> poses;
+    for (const auto& grasp : msg.grasps) {
+        geometry_msgs::Pose pose;
+        pose.position = grasp.position;
+        poses.push_back(pose);
+    }
+    // gpd_ros::GraspConfig first = msg;
+    // geometry_msgs::Pose pose;
+    // pose.position = first.position;
     std::cout << "received the message" << std::endl;
-    return pose;
+    return poses;
 }
 void move_arm(const gpd_ros::GraspConfigList& msg) {
     std::cout << "inside the move arm function";
     static const std::string PLANNING_GROUP = "arm";
     static moveit::planning_interface::MoveGroupInterface move_group(
         PLANNING_GROUP);
-    geometry_msgs::Pose pose = convert_message(msg);
-    move_group.setPoseTarget(pose);
-    moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-    bool success = (move_group.plan(my_plan) ==
-                    moveit::planning_interface::MoveItErrorCode::SUCCESS);
-    if (success) {
-        move_group.move();
-    } else {
-        std::cout << "not working" << std::endl;
+    std::vector<geometry_msgs::Pose> poses = convert_message(msg);
+    for (const geometry_msgs::Pose& pose: poses) {
+        move_group.setPoseTarget(pose);
+        moveit::planning_interface::MoveGroupInterface::Plan my_plan;
+        bool success = (move_group.plan(my_plan) ==
+                        moveit::planning_interface::MoveItErrorCode::SUCCESS);
+        if (success) {
+            move_group.move();
+            break;
+        } else {
+            std::cout << "not working" << std::endl;
+        }
     }
 }
 
