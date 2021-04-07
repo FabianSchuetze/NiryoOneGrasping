eefc9e659393a6e9108f15091ec07cecc718e258
FabianSchuetze <fabian.schutze@eui.eu>
2021-04-07 14:33:54 +0200

    maybe working

--- ObjectPose/include/pose_estimation.hpp
+++ ObjectPose/include/pose_estimation.hpp
@@ -1,30 +1,45 @@
 #ifndef pose_estimation_hpp
 #define pose_estimation_hpp
+#include <filesystem>
 #include <memory>
 #include <open3d/Open3D.h>
 #include <pcl/point_types.h>
 #include <pcl_ros/point_cloud.h>
 #include <vector>
-#include <filesystem>
 namespace o3d = open3d;
 namespace ObjectPose {
 typedef o3d::geometry::PointCloud Pointcloud;
-//typedef std::shared_ptr<o3d::geometry::PointCloud> PointCloud::Ptr;
+// typedef std::shared_ptr<o3d::geometry::PointCloud> PointCloud::Ptr;
 using Ptr = std::shared_ptr<o3d::geometry::PointCloud>;
 class PoseEstimation {
   public:
-    PoseEstimation(const std::filesystem::path&);
+    PoseEstimation(const std::filesystem::path &);
     void callback(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &);
 
   private:
+    struct BestResult {
+        int source_idx = -1;
+        int target_idx = -1;
+        o3d::pipelines::registration::RegistrationResult result;
+        Ptr source;
+        Ptr target;
+    };
     void cluster();
     void poses();
     std::vector<Ptr> clusters;
     std::vector<Ptr> meshes;
     static std::shared_ptr<o3d::geometry::PointCloud>
     toOpen3DPointCloud(const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &);
-    void findCluster(const Ptr&);
-    void readMeshes(const std::filesystem::path&);
+    void findCluster(const Ptr &);
+    void readMeshes(const std::filesystem::path &);
+    static o3d::pipelines::registration::RegistrationResult globalRegistration(
+        const Ptr &, const Ptr &,
+        const std::shared_ptr<o3d::pipelines::registration::Feature> &,
+        const std::shared_ptr<o3d::pipelines::registration::Feature> &);
+    o3d::pipelines::registration::RegistrationResult
+    estimateTransformation(const Ptr &, const Ptr &);
+    BestResult estimateTransformations(std::vector<Ptr> &, std::vector<Ptr> &);
+    void estimateTransformations();
 };
 } // namespace ObjectPose
 #endif
--- ObjectPose/src/pose_estimation.cpp
+++ ObjectPose/src/pose_estimation.cpp
@@ -3,24 +3,50 @@
 #include <algorithm>
 #include <sstream>
 
-//typedef o3d::geometry::PointCloud Pointcloud;
+// typedef o3d::geometry::PointCloud Pointcloud;
 ////typedef std::shared_ptr<o3d::geometry::PointCloud> PointCloud::Ptr;
-//using Ptr = std::shared_ptr<o3d::geometry::PointCloud>;
+// using Ptr = std::shared_ptr<o3d::geometry::PointCloud>;
+//
+static constexpr std::size_t N_POINTS(1000);
+static constexpr std::size_t MAX_UINT(255);
+using o3d::pipelines::registration::RegistrationResult;
+
+// struct BestResult {
+// int source_idx = -1;
+// int target_idx = -1;
+// RegistrationResult result;
+// ObjectPose::Ptr source;
+// ObjectPose::Ptr target;
+//};
+
+void VisualizeRegistration(const open3d::geometry::PointCloud &source,
+                           const open3d::geometry::PointCloud &target,
+                           const Eigen::Matrix4d &Transformation) {
+    std::shared_ptr<o3d::geometry::PointCloud> source_transformed_ptr(
+        new o3d::geometry::PointCloud);
+    std::shared_ptr<o3d::geometry::PointCloud> target_ptr(
+        new o3d::geometry::PointCloud);
+    *source_transformed_ptr = source;
+    *target_ptr = target;
+    source_transformed_ptr->Transform(Transformation);
+    o3d::visualization::DrawGeometries({source_transformed_ptr, target_ptr},
+                                       "Registration result");
+}
 
 Eigen::Vector3d inline convert_color(const pcl::PointXYZRGB &point) {
-    double r = static_cast<double>(point.r) / 255;
-    double g = static_cast<double>(point.g) / 255;
-    double b = static_cast<double>(point.b) / 255;
+    double r = static_cast<double>(point.r) / MAX_UINT;
+    double g = static_cast<double>(point.g) / MAX_UINT;
+    double b = static_cast<double>(point.b) / MAX_UINT;
     return {r, g, b};
 }
 
 namespace ObjectPose {
 
-void PoseEstimation::readMeshes(const std::filesystem::path& path) {
+void PoseEstimation::readMeshes(const std::filesystem::path &path) {
     std::string line;
     std::ifstream file(path);
     if (!file.is_open()) {
-        std::runtime_error("Files can't be opened");
+        throw std::runtime_error("Files can't be opened");
     }
     while (std::getline(file, line)) {
         o3d::geometry::TriangleMesh mesh;
@@ -28,12 +54,14 @@ void PoseEstimation::readMeshes(const std::filesystem::path& path) {
         if (!success) {
             throw std::runtime_error("Could not read mesh file");
         }
-        auto pcl = mesh.SamplePointsUniformly(1000);
+        auto pcl = mesh.SamplePointsUniformly(N_POINTS);
+        pcl->EstimateNormals();
         meshes.push_back(pcl);
     }
 }
 
-PoseEstimation::PoseEstimation(const std::filesystem::path& path) {
+PoseEstimation::PoseEstimation(const std::filesystem::path &path) {
+    readMeshes(path);
 }
 
 std::shared_ptr<o3d::geometry::PointCloud> PoseEstimation::toOpen3DPointCloud(
@@ -51,7 +79,7 @@ std::shared_ptr<o3d::geometry::PointCloud> PoseEstimation::toOpen3DPointCloud(
     return o3d_cloud;
 }
 
-void PoseEstimation::findCluster(const Ptr& source) {
+void PoseEstimation::findCluster(const Ptr &source) {
     std::vector<int> indices = source->ClusterDBSCAN(0.02, 100, false);
     auto it = std::max_element(indices.begin(), indices.end());
     if (*it == 0) {
@@ -65,12 +93,103 @@ void PoseEstimation::findCluster(const Ptr& source) {
         clusters[cluster]->points_.push_back(point);
         clusters[cluster]->colors_.push_back(color);
     }
+    std::for_each(clusters.begin(), clusters.end(),
+                  [](auto &x) { x->EstimateNormals(); });
+    // for (auto& cluster : clusters) {
+    // cluster->EstimateNormals();
+    //}
+}
+
+RegistrationResult PoseEstimation::globalRegistration(
+    const Ptr &source, const Ptr &target,
+    const std::shared_ptr<o3d::pipelines::registration::Feature> &source_fpfh,
+    const std::shared_ptr<o3d::pipelines::registration::Feature> &target_fpfh) {
+    std::vector<std::reference_wrapper<
+        const o3d::pipelines::registration::CorrespondenceChecker>>
+        correspondence_checker;
+    auto correspondence_checker_edge_length =
+        o3d::pipelines::registration::CorrespondenceCheckerBasedOnEdgeLength(
+            0.9);
+    auto correspondence_checker_distance =
+        o3d::pipelines::registration::CorrespondenceCheckerBasedOnDistance(
+            1.5 * 0.75);
+    // auto correspondence_checker_normal =
+    // pipelines::registration::CorrespondenceCheckerBasedOnNormal(
+    // 0.52359878);
+    correspondence_checker.emplace_back(correspondence_checker_edge_length);
+    correspondence_checker.emplace_back(correspondence_checker_distance);
+    bool mutual_filter(true);
+    auto result =
+        o3d::pipelines::registration::RegistrationRANSACBasedOnFeatureMatching(
+            *source, *target, *source_fpfh, *target_fpfh, mutual_filter,
+            1.5 * 0.75,
+            o3d::pipelines::registration::TransformationEstimationPointToPoint(
+                false),
+            4, correspondence_checker,
+            o3d::pipelines::registration::RANSACConvergenceCriteria(5000000,
+                                                                    0.9999));
+    return result;
+}
+
+RegistrationResult PoseEstimation::estimateTransformation(const Ptr &source,
+                                                          const Ptr &target) {
+    const auto source_features =
+        o3d::pipelines::registration::ComputeFPFHFeature(
+            *source, o3d::geometry::KDTreeSearchParamHybrid(0.05, 100));
+    const auto target_features =
+        o3d::pipelines::registration::ComputeFPFHFeature(
+            *target, o3d::geometry::KDTreeSearchParamHybrid(0.05, 100));
+    auto result =
+        globalRegistration(source, target, source_features, target_features);
+    return result;
+}
+
+PoseEstimation::BestResult
+PoseEstimation::estimateTransformations(std::vector<Ptr> &sources,
+                                        std::vector<Ptr> &targets) {
+    double best(0.3);
+    BestResult best_result;
+    for (std::size_t target_idx = 0; target_idx < meshes.size(); ++target_idx) {
+        const auto potential_target = targets[target_idx];
+        for (std::size_t source_idx = 0; source_idx < clusters.size();
+             ++source_idx) {
+            const auto source = sources[source_idx];
+            auto result = estimateTransformation(source, potential_target);
+            if (result.fitness_ > best) {
+                best_result.source_idx = source_idx;
+                best_result.target_idx = target_idx;
+                best_result.result = result;
+                best_result.source = source;
+                best_result.target = potential_target;
+                sources.erase(sources.begin() + source_idx);
+                targets.erase(targets.begin() + target_idx);
+                best = result.fitness_;
+            }
+        }
+    }
+    return best_result;
+}
+
+void PoseEstimation::estimateTransformations() {
+    std::vector<Ptr> sources(meshes.begin(), meshes.end());
+    std::vector<Ptr> targets(clusters.begin(), clusters.end());
+    bool found(false);
+    do {
+        BestResult result = estimateTransformations(sources, targets);
+        found = (result.source_idx != -1) & (result.target_idx != -1);
+        if (found) {
+            VisualizeRegistration(*result.source, *result.target,
+                                  result.result.transformation_);
+        }
+        // visualize the result
+    } while (found);
 }
 
 void PoseEstimation::callback(
     const pcl::PointCloud<pcl::PointXYZRGB>::ConstPtr &input) {
     auto pcl = toOpen3DPointCloud(input);
     findCluster(pcl);
+    estimateTransformations();
     // ROS_WARN_STREAM("The frame is " << frame);
 }
 } // namespace ObjectPose
