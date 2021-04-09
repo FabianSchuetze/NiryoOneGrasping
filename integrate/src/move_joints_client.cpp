ba204c0b60b81d489c3e11274c12e0cab6b3ad11
FabianSchuetze <fabian.schutze@eui.eu>
2021-04-09 18:06:23 +0200

    simple move_joints_client

---
+++ integrate/src/move_joints_client.cpp
@@ -0,0 +1,31 @@
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+#include <ros/ros.h>
+#include <pick_place/MoveJointsAction.h>
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "test_fibonacci");
+
+    // create the action client
+    // true causes the client to spin its own thread
+    actionlib::SimpleActionClient<pick_place::MoveJointsAction> ac("fibonacci", true);
+    ros::NodeHandle nh;
+
+    ROS_INFO("Waiting for action server to start.");
+    // wait for the action server to start
+    ac.waitForServer();  // will wait for infinite time
+
+    ROS_INFO("Action server started, sending goal.");
+    // send a goal to the action
+    pick_place::MoveJointsGoal goal;
+    ac.sendGoal(goal);
+    bool finished_before_timeout = ac.waitForResult(ros::Duration(30.0));
+    if (finished_before_timeout) {
+        actionlib::SimpleClientGoalState state = ac.getState();
+        ROS_WARN_STREAM("Action finished: " << state.toString());
+    } else
+        ROS_INFO("Action did not finish before the time out.");
+
+    // exit
+    return 0;
+}
+
