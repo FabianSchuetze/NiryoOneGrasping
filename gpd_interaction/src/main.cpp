3e5798a56d0067acd54df25c48b60bc8af8a91da
FabianSchuetze <fabian.schutze@eui.eu>
2021-04-21 14:37:34 +0200

    seems to be working better

--- gpd_interaction/src/main.cpp
+++ gpd_interaction/src/main.cpp
@@ -18,7 +18,7 @@
 #include <thread>
 
 using param = std::pair<std::string, std::string>;
-using PointCloud = pcl::PointCloud<pcl::PointXYZ>;
+using PointCloud = pcl::PointCloud<pcl::PointXYZRGB>;
 namespace fs = std::filesystem;
 constexpr std::size_t QUEUE(10);
 constexpr double PI(3.14);
@@ -38,16 +38,47 @@ void readParameters(const ros::NodeHandle &nh, T &... args) {
     (..., read_parameters(args));
 }
 
-double obtain_yaw(const geometry_msgs::Quaternion &quat) {
+
+double calculateRoll(const Eigen::Quaterniond &quat) {
+
+    //Eigen::Matrix3d tmp = transformation.matrix().block(0, 0, 3, 3);
+    //Eigen::Quaternion<double> quat(tmp);
+    double first = 2 * (quat.w() * quat.x() + quat.y() * quat.z());
+    double second = 1 - 2 * (quat.x() * quat.x() + quat.z() * quat.z());
+    double roll = std::atan2(first, second);
+    return roll;
+}
+double calculateYaw(const Eigen::Quaterniond &quat) {
+
+    //Eigen::Matrix3d tmp = transformation.matrix().block(0, 0, 3, 3);
+    //Eigen::Quaternion<double> quat(tmp);
+    double first = 2 * (quat.w() * quat.z() + quat.x() * quat.y());
+    double second = 1 - 2 * (quat.x() * quat.x() + quat.z() * quat.z());
+    double yaw = std::atan2(first, second);
+    return yaw;
+}
+
+double calculateYaw(const geometry_msgs::Quaternion &quat) {
     double first = 2 * (quat.w * quat.z + quat.x * quat.y);
     double second = 1 - 2 * (quat.x * quat.x + quat.z * quat.z);
     double yaw = std::atan2(first, second);
-    // if (yaw > PI / 2.0) {
-    // yaw = yaw - PI;
-    //} else if (yaw < -PI / 2.0) {
-    // yaw = yaw + PI;
-    //} // angles are close to zero;
     return yaw;
+}
+
+double calculatePitch(const Eigen::Quaterniond &quat) {
+
+    double first = 2 * (quat.w() * quat.y() - quat.y() * quat.x());
+    double pitch = std::asin(first);
+    return pitch;
+}
+
+std::tuple<double, double, double> RPY(const Eigen::Isometry3d &transform) {
+    Eigen::Matrix3d tmp = transform.matrix().block(0, 0, 3, 3);
+    Eigen::Quaternion<double> quat(tmp);
+    double roll = calculateRoll(quat);
+    double pitch = calculatePitch(quat);
+    double yaw = calculateYaw(quat);
+    return {roll, pitch, yaw};
 }
 
 Eigen::Isometry3d generateTransformation(const geometry_msgs::Pose &pose,
@@ -75,11 +106,9 @@ class SubscribeAndPublish {
         publish_pointcloud_ =
             n_.advertise<pcl::PointCloud<pcl::PointXYZ>>("/cloud_pcd", 1);
     }
-
-    void callback_grasp_pose(const gpd_ros::GraspConfigList &msg) {
-        ROS_WARN_STREAM("Inside the grap pose callback");
-        const std::string name = msg.header.frame_id;
-        const gpd_ros::GraspConfig &grasp = msg.grasps[0];
+    Eigen::Isometry3d
+    convertHandToTransform(const gpd_ros::GraspConfig &grasp) {
+        Eigen::Isometry3d trans;
         Eigen::Matrix4d frame = Eigen::MatrixXd::Identity(4, 4);
         Eigen::Vector3d approach, binormal, axis, position;
         tf2::fromMsg(grasp.approach, approach);
@@ -90,20 +119,30 @@ class SubscribeAndPublish {
         frame.block<3, 1>(0, 1) = binormal;
         frame.block<3, 1>(0, 2) = axis;
         frame.block<3, 1>(0, 3) = position;
-        ros_transform.matrix() = frame;
+        trans.matrix() = frame;
+        return trans;
+    }
+
+    void callback_grasp_pose(const gpd_ros::GraspConfigList &msg) {
+        possible_transforms.clear();
+        for (auto const &grasp : msg.grasps) {
+            auto possible_transform = convertHandToTransform(grasp);
+            possible_transforms.push_back(possible_transform);
+        }
         grasp_pose_received = true;
     }
 
-    pcl::PointXYZ inline toPointXYZ(const Eigen::Vector3d &point) {
-        pcl::PointXYZ pcl_point;
+    pcl::PointXYZRGB inline toPointXYZRGB(const Eigen::Vector3d &point) {
+        pcl::PointXYZRGB pcl_point;
         pcl_point.x = static_cast<float>(point(0));
         pcl_point.y = static_cast<float>(point(1));
         pcl_point.z = static_cast<float>(point(2));
-        // pcl_point.r = static_cast<uint8_t>(std::round(color(0) * MAX_UINT));
-        // pcl_point.g = static_cast<uint8_t>(std::round(color(1) * MAX_UINT));
-        // pcl_point.b = static_cast<uint8_t>(std::round(color(2) * MAX_UINT));
+        pcl_point.r = static_cast<uint8_t>(0);
+        pcl_point.g = static_cast<uint8_t>(0);
+        pcl_point.b = static_cast<uint8_t>(255);
         return pcl_point;
     }
+
     std::string shortName(const std::string &input_name,
                           std::string extension) {
         const std::string fn = fs::path(input_name).filename();
@@ -121,58 +160,108 @@ class SubscribeAndPublish {
         }
         auto cloud = mesh.SamplePointsUniformly(50000);
         PointCloud::Ptr pcl_cloud(new PointCloud);
-        const std::vector<Eigen::Vector3d> &points = cloud->points_;
-        const std::vector<Eigen::Vector3d> &colors = cloud->colors_;
-        for (std::size_t idx = 0; idx < points.size(); ++idx) {
-            auto point = toPointXYZ(points[idx]);
+        for (const auto &o3d_point : cloud->points_) {
+            auto point = toPointXYZRGB(o3d_point);
             pcl_cloud->push_back(point);
         }
         pcl_cloud->height = 1;
-        pcl_cloud->width = points.size();
+        pcl_cloud->width = cloud->points_.size();
         std::string fn = fs::path(name).filename();
         pcl_cloud->header.frame_id = fn;
         publish_pointcloud_.publish(pcl_cloud);
     }
 
+    void
+    publishTransform(const Eigen::Isometry3d &transform,
+                     const std::string &name, std::string extension,
+                     std::vector<geometry_msgs::TransformStamped> &transforms) {
+        auto tmp = tf2::eigenToTransform(transform);
+        tmp.header.frame_id = "base_link";
+        tmp.child_frame_id = shortName(name, extension);
+        transforms.push_back(tmp);
+    }
+
+    bool feasible(const Eigen::Isometry3d &frame) {
+        Eigen::MatrixXd gripper_frame = Eigen::MatrixXd::Identity(4, 4);
+        gripper_frame(0, 3) = 0.12;
+        Eigen::Matrix4d result;
+        result = frame.matrix() * gripper_frame;
+        bool is_feasible = (result(2, 3) > 0.02);
+        return is_feasible;
+    }
+
+    Eigen::Isometry3d
+    filterPossibleTransforms(const Eigen::Isometry3d &object) {
+        Eigen::Isometry3d best_hand;
+        double error = 1000;
+        for (const auto &possible_transform : possible_transforms) {
+            const auto hand = object * possible_transform;
+            if (!feasible(hand)) {
+                if (hand.matrix()(2,3) > 0.01) {
+                    return hand;
+                }
+                continue;
+            }
+            std::cout << "feasible" << std::endl;
+            auto [roll_object, pitch_object, yaw_object] = RPY(object);
+            auto [roll_hand, pitch_hand, yaw_hand] = RPY(hand);
+            double diff_yaw = std::abs(yaw_object - yaw_hand);
+            double diff_roll = std::abs(roll_object - roll_hand);
+            double curr_error = diff_yaw + diff_roll;
+            if (curr_error < error) {
+                std::cout << "yaw_object and hand" << yaw_hand  << ", " <<
+                    yaw_object << std::endl;
+                best_hand = possible_transform;
+                error = curr_error;
+            }
+        }
+        return best_hand;
+    }
+
     void callback_object_pose(const object_pose::positions &msg) {
         grasp_pose_received = false;
         ROS_WARN_STREAM("Inside the object pose callback");
-        const auto now = ros::Time::now();
+        // const auto now = ros::Time::now();
         std::vector<geometry_msgs::TransformStamped> transforms;
-        geometry_msgs::PoseArray poses;
-        poses.header.frame_id = msg.poses.header.frame_id;
-        poses.header.stamp = now;
+        // geometry_msgs::PoseArray poses;
+        // poses.header.frame_id = msg.poses.header.frame_id;
+        // poses.header.stamp = now;
         for (std::size_t i = 0; i < msg.objects.size(); ++i) {
             grasp_pose_received = false;
             geometry_msgs::Pose pose = msg.poses.poses[i];
             const std::string &name = msg.objects[i];
-            double yaw = obtain_yaw(pose.orientation);
-            Eigen::Isometry3d transformStamped =
-                generateTransformation(pose, yaw);
-            auto transform = tf2::eigenToTransform(transformStamped);
-            transform.header.frame_id = "base_link";
-            transform.child_frame_id = shortName(name, "");
+            double yaw = calculateYaw(pose.orientation);
+            Eigen::Isometry3d object_frame = generateTransformation(pose, yaw);
+            publishTransform(object_frame, name, "", transforms);
             publishPointCloud(name);
             while ((!grasp_pose_received) and ros::ok()) {
                 std::this_thread::sleep_for(std::chrono::seconds(1));
             }
             grasp_pose_received = false;
-            Eigen::Isometry3d tmp = transformStamped * ros_transform;
-            auto final_transform = tf2::eigenToTransform(tmp);
-            final_transform.child_frame_id = shortName(name, "_grasp");
-            final_transform.header.frame_id = "base_link";
-            transforms.push_back(transform);
-            transforms.push_back(final_transform);
+            int j = 0;
+            for (const auto& hand : possible_transforms) {
+            //Eigen::Isometry3d hand = filterPossibleTransforms(object_frame);
+                Eigen::Isometry3d tmp = object_frame * hand;
+                publishTransform(tmp, name, "_grasp" + std::to_string(j), transforms);
+                j++;
+                if (j > 9) {
+                    break;
+                }
+            }
+            // auto final_transform = tf2::eigenToTransform(tmp);
+            // final_transform.child_frame_id = shortName(name, "_grasp");
+            // final_transform.header.frame_id = "base_link";
+            // transforms.push_back(final_transform);
         }
         ROS_WARN_STREAM("Transfroms size: " << transforms.size());
-        pub_.publish(poses);
+        // pub_.publish(poses);
         br.sendTransform(transforms);
     }
 
   private:
     ros::Publisher pub_;
     ros::Publisher publish_pointcloud_;
-    Eigen::Isometry3d ros_transform;
+    std::vector<Eigen::Isometry3d> possible_transforms;
     tf2_ros::StaticTransformBroadcaster br;
     bool grasp_pose_received;
 }; // End of class SubscribeAndPublisL
